96c1556198d424325d23b1dcb67be38271ddc8c4
zoyazee <joygathigira@gmail.com>
2019-10-17 18:09:53 +0300

    My dashboard react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Content from './components/Content'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Router>
+          <Sidebar />
+          <Route path='/' component={Content} />
+        </Router>
+      </div>
+    )
+  }
+}
+
+
+export default App;
